ae2848d7294d9cd58f4b12a1163530f1622836b2
Glymaxus <bastiengiat@gmail.com>
2023-03-21 16:39:48 +0100

    Initial Commit

---
+++ LibertyBookOfficiel/LibertyBookOfficielApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibertyBookOfficielApp.swift
+//  LibertyBookOfficiel
+//
+//  Created by bastien giat on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LibertyBookOfficielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
