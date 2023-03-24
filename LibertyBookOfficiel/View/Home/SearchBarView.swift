d4851dd75165a247e7010f0ac458015af1a0e904
Glymaxus <bastiengiat@gmail.com>
2023-03-24 19:14:10 +0100

    User Page, Search Bar et Background color

---
+++ LibertyBookOfficiel/View/Home/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  LibertyBookOfficiel
+//
+//  Created by bastien giat on 24/03/2023.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}
