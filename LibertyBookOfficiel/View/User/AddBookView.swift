4c6416f64a0f4431706c43d0bb2f5349400d7521
Glymaxus <bastiengiat@gmail.com>
2023-03-27 18:52:08 +0200

    Modification userPage + début addBookPageView

---
+++ LibertyBookOfficiel/View/User/AddBookView.swift
@@ -0,0 +1,20 @@
+//
+//  AddBookView.swift
+//  LibertyBookOfficiel
+//
+//  Created by bastien giat on 27/03/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
